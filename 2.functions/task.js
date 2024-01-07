6ea0d1344b466b9b49f560dd873882c47fe31d7b
hellovadikv <vadimlaitman@gmail.com>
2024-01-07 15:49:12 +0500

    Выполнил задание fuctions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,51 @@
-function getArrayParams(...arr) {
+"use strict"
 
-  return { min: min, max: max, avg: avg };
+function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0
+
+  arr.forEach((i) => {
+    if (i < min) min = i
+  })
+  arr.forEach((i) => {
+    if (i > max) max = i
+  })
+  arr.forEach(i => sum += i)
+
+  return { min: min, max: max, avg: Number((sum / arr.length).toFixed(2)) }
 }
+  
 
 function summElementsWorker(...arr) {
-
+  arr.reduce((sum, i) => sum + i, 0)
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  return Math.max.apply(null, arr) - Math.min.apply(null, arr)
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  return arr.filter(i => i % 2 === 0).reduce((a, b) => a + b, 0) - arr.filter(i => i % 2 !== 0).reduce((a, b) => a + b, 0)
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  let evens = arr.filter(i => i % 2 === 0)
+  return evens.reduce((a, b) => a + b, 0) / evens.length
 }
 
 function makeWork (arrOfArr, func) {
-
+  let max = -Infinity
+  arr.forEach(i => {
+    let result = func(...i)
+    if (result > max) max = result
+  })
+  return max
 }
+
+
+
+
+
+
+
