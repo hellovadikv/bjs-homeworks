1f7d483909e321b37d6ffd9eafa91529fff2f4c1
hellovadikv <vadimlaitman@gmail.com>
2024-01-07 15:55:55 +0500

    Исправил функцию summElementsWorkers

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -18,23 +18,28 @@ function getArrayParams(...arr) {
   
 
 function summElementsWorker(...arr) {
-  arr.reduce((sum, i) => sum + i, 0)
+  if (arr.length === 0) return 0
+  return arr.reduce((a, b) => a + b, 0)
 }
 
 function differenceMaxMinWorker(...arr) {
+  if (arr.length === 0) return 0
   return Math.max.apply(null, arr) - Math.min.apply(null, arr)
 }
 
 function differenceEvenOddWorker(...arr) {
+  if (arr.length === 0) return 0
   return arr.filter(i => i % 2 === 0).reduce((a, b) => a + b, 0) - arr.filter(i => i % 2 !== 0).reduce((a, b) => a + b, 0)
 }
 
 function averageEvenElementsWorker(...arr) {
+  if (arr.length === 0) return 0
   let evens = arr.filter(i => i % 2 === 0)
   return evens.reduce((a, b) => a + b, 0) / evens.length
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork (arr, func) {
+  if (arr.length === 0) return 0
   let max = -Infinity
   arr.forEach(i => {
     let result = func(...i)
@@ -43,9 +48,7 @@ function makeWork (arrOfArr, func) {
   return max
 }
 
-
-
-
+ 
 
 
 
